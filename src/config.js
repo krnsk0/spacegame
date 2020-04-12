58c9e61a4bd9bb77edc059448080dde9483ef7c1
krnsk0 <jkurinsky@gmail.com>
2020-04-12 18:46:40 -0400

    update config

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-export const WIDTH = 310;
-export const HEIGHT = 640;
-export const STAR_SIZE = 5; // pixels
-export const STAR_STEP = 5; // pixels
-export const STAR_CHANCE = 0.5; // 0 to 1
+export const WIDTH = 1280;
+export const HEIGHT = 720;
+export const STAR_SIZE = 4; // pixels
+export const STAR_STEP = 10; // pixels
+export const STAR_CHANCE = 0.3; // 0 to 1
 export const BASE_STAR_INTENSITY = 0.7; // 0 to 1
