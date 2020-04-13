48457abb5f12b7c180327644338a49c6b828c2e8
Jon Kurinsky <jkurinsky@gmail.com>
2020-04-12 22:49:31 -0400

    wip commit

--- src/config.js
+++ src/config.js
@@ -5,7 +5,9 @@ export const STAR_SPEED = 500; // pixels/sec
 export const STAR_CHANCE = 0.15; // 0 to 1
 export const BASE_STAR_INTENSITY = 0.6; // 0 to 1
 export const PLAYER_SIZE = 10; // pixels
-export const PLAYER_SPEED = 300; // pixels/sec
+export const PLAYER_BASE_SPEED = 300; // pixels/sec
+export const PLAYER_ACCELERATION = 0.2;
+export const PLAYER_INERTIA = 0.1;
 
 export const DIRS = {
   UP: 'U',
--- src/store/playerModule.js
+++ src/store/playerModule.js
@@ -1,24 +1,41 @@
-import { HEIGHT, WIDTH, PLAYER_SIZE, PLAYER_SPEED, DIRS } from '../config';
+import {
+  HEIGHT,
+  WIDTH,
+  PLAYER_SIZE,
+  PLAYER_BASE_SPEED,
+  DIRS,
+  PLAYER_INERTIA,
+  PLAYER_ACCELERATION,
+} from '../config';
 
 const MOVE_PLAYER = 'MOVE_PLAYER';
 const UPDATE_KEY_STATE = 'UPDATE_KEY_STATE';
+const UPDATE_PLAYER_VELOCITY = 'UPDATE_PLAYER_VELOCITY';
 
 export const playerTick = (delta) => {
   return (dispatch, getState) => {
     const {
-      player: { keys },
+      player: { xVel, yVel, keys },
     } = getState();
 
-    const sec = delta * 0.001;
-    const baseMoveInPixels = PLAYER_SPEED * sec;
-    const dx =
-      (keys[DIRS.LEFT] ? -baseMoveInPixels : 0) +
-      (keys[DIRS.RIGHT] ? baseMoveInPixels : 0);
-    const dy =
-      (keys[DIRS.UP] ? -baseMoveInPixels : 0) +
-      (keys[DIRS.DOWN] ? baseMoveInPixels : 0);
+    // const sec = delta * 0.001;
+    // const baseMoveInPixels = PLAYER_BASE_SPEED * sec;
 
+    const dx = xVel;
+    const dy = yVel;
     dispatch(movePlayer({ dx, dy }));
+
+    const newXVel = xVel;
+    // xVel -
+    // PLAYER_INERTIA +
+    // (keys[DIRS.LEFT] ? -PLAYER_ACCELERATION : 0) +
+    // (keys[DIRS.RIGHT] ? PLAYER_ACCELERATION : 0);
+    const newYVel = yVel;
+    // yVel -
+    // PLAYER_INERTIA +
+    // (keys[DIRS.UP] ? -PLAYER_ACCELERATION : 0) +
+    // (keys[DIRS.DOWN] ? PLAYER_ACCELERATION : 0);
+    dispatch(updatePlayerVelocity({ xVel: newXVel, yVel: newYVel }));
   };
 };
 
@@ -26,6 +43,12 @@ export const movePlayer = ({ dx, dy }) => ({
   type: MOVE_PLAYER,
   dx,
   dy,
+});
+
+export const updatePlayerVelocity = ({ xVel, yVel }) => ({
+  type: UPDATE_PLAYER_VELOCITY,
+  xVel,
+  yVel,
 });
 
 export const updateKey = ({ direction, keyState }) => ({
@@ -37,6 +60,8 @@ export const updateKey = ({ direction, keyState }) => ({
 const initialState = {
   x: Math.floor(WIDTH / 2 - PLAYER_SIZE / 2),
   y: Math.floor(HEIGHT - PLAYER_SIZE * 2),
+  xVel: 0,
+  yVel: 0,
   keys: {
     [DIRS.UP]: false,
     [DIRS.DOWN]: false,
@@ -52,6 +77,13 @@ export const playerReducer = (state = initialState, action) => {
         ...state,
         x: state.x + action.dx,
         y: state.y + action.dy,
+      };
+    },
+    [UPDATE_PLAYER_VELOCITY]: () => {
+      return {
+        ...state,
+        xVel: action.xVel,
+        yVel: action.yVel,
       };
     },
     [UPDATE_KEY_STATE]: () => {
